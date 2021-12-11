83314a0c45fc1c68e0a80f1c60df3d17fbb74bdb
Sole Cold <cool.gegeg@gmail.com>
2021-12-11 23:17:44 +0200

    UI for ammo

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement _playerMovement;
+    [SerializeField] private PlayerShooting _playerShooting;
+}
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,7 +1,6 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Experimental.TerrainAPI;
-using UnityEngine.Serialization;
 
 public class PlayerShooting : MonoBehaviour
 {
@@ -9,8 +8,42 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField] private ObjectsPool _bulletsPool;
     [SerializeField, Min(0)] private int _bulletLifetime;
     [SerializeField, Min(0)] private float _bulletForce;
+    [SerializeField, Min(0)] private int _ammo;
+    [SerializeField] private TextMeshProUGUI _ammoIndicator;
+
 
     private void Update()
+    {
+        ProcessInput();
+        UpdateUI();
+    }
+
+    private void Shoot()
+    {
+        if (_ammo == 0)
+        {
+            return;
+        }
+
+        var bullet = _bulletsPool.GetObject();
+        bullet.transform.position = _firePoint.position;
+        bullet.transform.rotation = _firePoint.rotation;
+
+        bullet.GetComponent<Rigidbody2D>().AddForce(_firePoint.up * _bulletForce, ForceMode2D.Impulse);
+
+        _ammo -= 1;
+
+        StartCoroutine(BulletTimer(bullet));
+    }
+
+    private IEnumerator BulletTimer(GameObject obj)
+    {
+        yield return new WaitForSeconds(_bulletLifetime);
+
+        _bulletsPool.ReturnObject(obj);
+    }
+
+    private void ProcessInput()
     {
         if (Input.GetButtonDown("Fire1"))
         {
@@ -18,20 +51,8 @@ public class PlayerShooting : MonoBehaviour
         }
     }
 
-    private void Shoot()
+    private void UpdateUI()
     {
-        var bullet = _bulletsPool.GetObject();
-        bullet.transform.position = _firePoint.position;
-        bullet.transform.rotation = _firePoint.rotation;
-        
-        bullet.GetComponent<Rigidbody2D>().AddForce(_firePoint.up * _bulletForce, ForceMode2D.Impulse);
-        StartCoroutine(BulletTimer(bullet));
-    }
-
-    private IEnumerator BulletTimer(GameObject obj)
-    {
-        yield return new WaitForSeconds(_bulletLifetime);
-        
-        _bulletsPool.ReturnObject(obj);
+        _ammoIndicator.text = _ammo.ToString();
     }
 }
